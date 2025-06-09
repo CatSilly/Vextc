412d196067f49e7e504231c32b57c484f1df08f5
CatSilly <eventhonizonvn@gmail.com>
2025-06-09 13:07:39 +0700

    Add files via upload

---
+++ run.py
@@ -0,0 +1,13 @@
+#Chạy game nhấn nút vàng bên dưới
+#Một số thông tin đặt biệt vui lòng đọc file "README.md"
+#Tải trước thư viện colorama trước khi chạy
+
+from Menu_game import *
+
+main()
+display_menu()
+
+if __name__ == '__main___':
+	main()
+	display_menu()
+	
